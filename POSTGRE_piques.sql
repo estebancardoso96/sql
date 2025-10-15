baed22ce6912e83a6b644c9e3979c01254a93d26
Esteban Cardoso <99781288+estebancardoso96@users.noreply.github.com>
2025-10-15 11:55:56 -0300

    Update POSTGRE_piques.sql

--- POSTGRE_piques.sql
+++ POSTGRE_piques.sql
@@ -39,3 +39,7 @@ from poblacion.fact_vinculos fvbc
 where descripcion_cod is not null
 order by cod_causal
 
+-- Editar uno o más valores (edito la edad de la persona con id_persona igual a 35) de filas de una tabla --
+update esquema.tabla
+set edad = 35
+where id_persona = 2
